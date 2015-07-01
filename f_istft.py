77ecaf49ad7cc50eace9f309d180d6fe003dbc86
fpishdadian <fpishdadian@u.northwestern.edu>
2015-07-01 17:38:04 -0500

    STFT and ISTFT functions updated.

--- f_istft.py
+++ f_istft.py
@@ -1,22 +1,52 @@
-## This function computes the inverse STFT of a spectrogram using 
-# Overlap Adition method
-#
-# Inputs:
-# X: one-sided STFT (spectrogram) of the signal x
-# L: window length (default window type: Hamming)
-# win: window type, (string): Rectangular, Hamming, Hanning, Blackman
-# ovp: overlap between adjacent windows in STFT analysis
-# nfft: number of freq. samples in (-pi,pi]
-# fs: sampling rate of the original signal x
-#
-# Outputs:
-# t: vector of time values for the reconstructed signal
-# y: the reconstructed signal
+"""
+This function computes the inverse STFT of a spectrogram using 
+Overlap Adition method
 
-def f_istft(X,L,win,ovp,nfft,fs):
-    
+Inputs:
+X: one-sided STFT (spectrogram) of the signal x
+L: window length 
+win(optional): window type, (string): 'Rectangular', 'Hamming', 'Hanning', 'Blackman'
+   (default window type: Hamming)
+ovp: overlap between adjacent windows in STFT analysis
+nfft: number of freq. samples in (-pi,pi]
+fs: sampling rate of the original signal x
+
+Outputs:
+t: vector of time values for the reconstructed signal
+y: the reconstructed signal
+
+EXAMPLE:
+# first generate the spectrogram of a signal and then reconstruct the spectrogram back 
+to the time-domain (the spectrogram in real problems would go through further 
+processing, which for simplicity is not shown here) 
+  fs=16000
+  t=np.mat(np.arange(fs+1)/float(fs))
+  x=np.cos(2*np.pi*440*t)
+  L=1024
+  win='Hanning'
+  ovp=0.5*L
+  nfft=1024
+  mkplot=1
+  fmax=1000
+  S,P,F,T = f_stft(x,L,win,ovp,nfft,fs,mkplot,fmax)
+  y,t = f_istft(S,L,win,ovp,nfft,fs)
+  
+Required packages:
+1. Numpy
+2. Scipy
+
+"""
+
+def f_istft(*arg):
+   
     import numpy as np 
     from scipy.fftpack import ifft
+    
+    if len(arg)==5: 
+        X,L,ovp,nfft,fs = arg[0:5]
+        win='Hamming'
+    elif len(arg)==6:
+        X,L,win,ovp,nfft,fs = arg[0:6]
     
     # Get spectrogram dimenstions and compute window hop size
     Nc=X.shape[1] # number of columns of X
@@ -44,7 +74,8 @@ def f_istft(X,L,win,ovp,nfft,fs):
         y[0,hh:hh+L]=y[0,hh:hh+L]+yh[0:L]
         
     c=sum(W)/Hop
-    y=y/c
+    y=y[0,:]/c
+    #y=y[0,:]
     t=np.arange(Ly)/float(fs)
     
     return y,t
