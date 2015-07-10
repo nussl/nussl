726f2c6bdb7cf1cbe74a7e58801c0e933ca5fd95
fpishdadian <fpishdadian@u.northwestern.edu>
2015-07-10 09:09:37 -0500

    the DUET module and demo_duet testing code updated

--- demo_duet.py
+++ demo_duet.py
@@ -8,7 +8,8 @@ from f_stft import f_stft
 from DUET import duet
 
 # load the audio file
-x,fs,enc = wavread('dev1_female3_inst_mix.wav'); 
+#x,fs,enc = wavread('dev1_female3_inst_mix.wav'); 
+x,fs,enc = wavread('dev1_nodrums_inst_mix.wav'); 
 x=np.mat(x).T
 t=np.mat(np.arange(np.shape(x)[1])/float(fs))
 #play(x[0,:],fs)
@@ -19,19 +20,35 @@ win='Hamming'
 ovp=0.5*L
 nfft=L
 mkplot=1
-fmax=3000;
+fmax=3000
 
 plt.figure(1)
 plt.title('Mixture');
-Sm = f_stft(x[0,:],L,win,ovp,nfft,fs,mkplot,fmax); 
+X = f_stft(x[0,:],L,win,ovp,nfft,fs,mkplot,fmax); 
 plt.show()
 
 # compute and plot the 2D histogram of mixing parameters
 a_min=-3; a_max=3; a_num=50;
 d_min=-3; d_max=3; d_num=50;
 
-sparam = np.array([L,win,ovp,nfft,fs])
-adparam = np.array([-3,3,50,-3,3,50])
-Pr=np.array([1,2,3])
+sparam = np.array([(L,win,ovp,nfft,fs)],dtype=[('winlen',int),('wintype','|S10'),('overlap',int),('numfreq',int),('sampfreq',int)])
+adparam = np.array([(a_min,a_max,a_num,d_min,d_max,d_num)],dtype=[('amin',float),('amax',float)
+,('anum',int),('dmin',float),('dmax',float),('dnum',int)])
 
-hist=duet(x,sparam,adparam,Pr)
+thr=0.1; a_mindist=5; d_mindist=5; N=3;
+Pr=np.array([0.1,5,5,3])
+
+xhat,ad_est=duet(x,sparam,adparam,Pr,'y')
+
+# play the separated sources
+#for i in range(0,N):
+#  play(xhat[i,:],fs)
+ 
+
+
+
+
+
+
+
+
