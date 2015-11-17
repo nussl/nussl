7ea6d41780f7bd3a5ebafc1bbd4144525a183dc4
Ethan Manilow <eth@nmanilow.com>
2015-11-17 01:19:52 -0600

    Optimizing NMF. Euclidean distance is crazy fast now, divergence is faster but has room to grow. I'm going to throw theano into that to make it faster next.

--- Demos/demo_nmf.py
+++ Demos/demo_nmf.py
@@ -1,14 +1,17 @@
 import AudioSignal, random, time
 import numpy as np
 import Nmf as NmfNU
-import nimfa
+import nimfa, pprint
+import matplotlib.pylab as plt
+
 
 random.seed(1)
 
 
 def main():
     simpleExample()
-    audioExample()
+    #audioExample()
+    #sineExample()
 
 
 def simpleExample():
@@ -21,7 +24,7 @@ def simpleExample():
     print '-' * 60
 
     # Make two simple matrices
-    n = 3
+    n = 48
     a = np.arange(n ** 2).reshape((n, n))
     b = 2 * a + 3
 
@@ -38,7 +41,7 @@ def simpleExample():
     # Run NU NMF
     start = time.time()
     nmf.Run()
-    print '{0:.3f}'.format(time.time() - start), 'seconds for NUNMF'
+    print '{0:.3f}'.format(time.time() - start), 'seconds for NUSSL'
 
     # Set up and run nimfa NMF
     nmf2 = nimfa.Nmf(mixture, update='divergence', rank=2, max_iter=nmf.maxNumIterations)
@@ -64,7 +67,6 @@ def simpleExample():
     print '    ', '-' * 10, 'NIMFA ', '-' * 10
     print H
     print W
-    i = 1
 
 
 def audioExample():
@@ -82,15 +84,15 @@ def audioExample():
     numNotes = 2
 
     # Two input files
-    firstFileName = 'Input/K0140.wav'
-    secondFileName = 'Input/K0149.wav'
+    firstFileName = '../Input/K0140.wav'
+    secondFileName = '../Input/K0149.wav'
 
     firstNote = AudioSignal.AudioSignal(firstFileName)
     secondNote = AudioSignal.AudioSignal(secondFileName)
 
     # Combine notes into one file and save target
     bothNotesPre = firstNote + secondNote
-    bothNotesPre.WriteAudioFile('Output/combined_preNMF.wav')
+    bothNotesPre.WriteAudioFile('../Output/combined_preNMF.wav')
 
     bothNotesForNMF = firstNote + secondNote
     _, stft, _, _ = bothNotesForNMF.STFT()
@@ -116,12 +118,18 @@ def audioExample():
 
     # run NMF
     nmf = NmfNU.Nmf(stft, numNotes, activationMatrix=GuessAct, templateVectors=GuessVec)
+    nmf.maxNumIterations = 100
     start = time.time()
     nmf.Run()
-    print int(time.time() - start), 'sec'
+    print '{0:.3f}'.format(time.time() - start), 'seconds for NUSSL'
+
+    nmf2 = nimfa.Nmf(stft, rank=numNotes, max_iter=nmf.maxNumIterations)
+    start = time.time()
+    nmf2()
+    print '{0:.3f}'.format(time.time() - start), 'seconds for nimfa'
 
     # Make output files
-    outFileNameBase = 'Output/NMFoutput_'
+    outFileNameBase = '../Output/NMFoutput_'
     i = 1
     newSignals = nmf.MakeAudioSignals()
     for signal in newSignals:
@@ -131,7 +139,38 @@ def audioExample():
 
     # Recombine signals and make a new output file
     recombined = newSignals[0] + newSignals[1]
-    recombined.WriteAudioFile('Output/combined_postNMF.wav')
+    recombined.WriteAudioFile('../Output/combined_postNMF.wav')
+
+
+def sineExample():
+    nSamples = 44100 # 1 second per each frequency
+
+    sin1 = np.sin(np.linspace(0, 100*2*np.pi, nSamples))
+    sin2 = np.sin(np.linspace(0, 200*2*np.pi, nSamples))
+    sin3 = np.sin(np.linspace(0, 300*2*np.pi, nSamples))
+
+    sines = np.concatenate((sin1, sin2, sin3))
+
+
+    signal = AudioSignal.AudioSignal(timeSeries=sines)
+    _, stft, _, _ = signal.STFT()
+
+    start = time.time()
+    nmf = NmfNU.Nmf(stft, 3)
+    # nmf.distanceMeasure = NmfNU.DistanceType.Divergence
+    A, B = nmf.Run()
+    print '{0:.3f}'.format(time.time() - start), 'sec'
+
+    plt.plot(A.T)
+    plt.title(str(time.time()))
+    plt.savefig('../Output/A.png')
+    plt.close()
+
+    plt.plot(B)
+    plt.xlim([0,25])
+    plt.title(str(time.time()))
+    plt.savefig('../Output/B.png')
+
 
 
 if __name__ == '__main__':
