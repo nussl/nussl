92efa11167c6ae53643ff1ea6d194de38f6cc7e3
Ethan Manilow <eth@nmanilow.com>
2016-01-05 11:38:38 -0600

    Renaming DUET.py to Duet.py to match naming scheme

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ import FftUtils
 from WindowType import WindowType
 from WindowAttributes import WindowAttributes
 from AudioSignal import AudioSignal
-from DUET import Duet
+from Duet import Duet
 from Nmf import Nmf
 from Repet import Repet
