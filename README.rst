975bc28ab27860d8c52e3915f41e0fb7f149e124
Ethan Manilow <eth@nmanilow.com>
2016-01-12 13:51:51 -0600

    Starting making this pip-able. Added README.rst and setup.py

---
+++ README.rst
@@ -0,0 +1,20 @@
+-----
+-----
+nussl
+-----
+-----
+
+nussl (pronounced like "nuzzle") is a flexible python source separation library
+created by the Interactive Audio Lab at Northwestern University.
+
+
+Features
+--------
+
+At its core, nussl contains implementations of the following source separation algorithms:
+* DUET
+* REPET
+* NMF
+* KAM
+* RPCA
+nussle also has the capabilities to
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+
+requirements = [
+
+]
+
+setup(
+    name='nussl',
+    version='0.0.1',
+    description='A flexible source separation library',
+    long_description='',
+    author='C. Grief, E. Manilow, F. Pishdadian',
+    author_email='ethan.manilow@u.northwestern.edu',
+    url='https://github.com/interactiveaudiolab/SourceSeparation',
+    license=''
+)
