95b4ba4e71edb20f5e3483e3817e131c8bc04c4c
cegrief <cegrief@gmail.com>
2016-02-02 00:09:33 -0600

    updated examples slightly, added install instructions, and updated main page

--- Demos/demo_repet.py
+++ Demos/demo_repet.py
@@ -7,6 +7,7 @@ def main():
     inputName = '../Input/Sample1.wav'
     signal = nussl.AudioSignal(path_to_input_file=inputName)
 
+    # make a directory to store output if needed
     if not os.path.exists('../Output/'):
         os.mkdir('../Output')
 
@@ -15,13 +16,16 @@ def main():
     win.window_length = 2048
     win.window_type = nussl.WindowType.HAMMING
 
-    # Set up and run Repet
+    # Set up Repet
     repet = nussl.Repet(signal, repet_type=nussl.RepetType.SIM, window_attributes=win)
     repet.min_distance_between_frames = 0.1
+    # and Run
     repet.run()
 
-    # Get audio signals and write out to files
+    # Get foreground and backgroun audio signals
     bkgd, fgnd = repet.make_audio_signals()
+
+    # and write out to files
     bkgd.write_audio_to_file(os.path.join('..', 'Output', 'repet_background.wav'))
     fgnd.write_audio_to_file(os.path.join('..', 'Output', 'repet_foreground.wav'))
 
