08fa99c4b02c5be775ebb1a5c0c473e2a912e74e
Ethan Manilow <eth@nmanilow.com>
2016-03-11 01:09:42 -0600

    Also adding both find_repeating_period functions and demo

--- Demos/demo_repet_find_repeating.py
+++ Demos/demo_repet_find_repeating.py
@@ -1,18 +1,21 @@
 import nussl
 
 def main():
-    a = nussl.AudioSignal('../Input/saw1_noise_time_0.05.wav')
+    a = nussl.AudioSignal('../Input/HistoryRepeating_PropellorHeads.wav')
     r = nussl.Repet(a)
 
     beat_spectrum = r.get_beat_spectrum()
 
     r.update_periods()
-    repeating_period = r.find_repeating_period_simple(beat_spectrum, r.min_period, r.max_period)
+    period_simple = r.find_repeating_period_simple(beat_spectrum, r.min_period, r.max_period)
+    period_complex = float(r.find_repeating_period_complex(beat_spectrum))
 
     # repeating_period based on the stft so is a multiple of hop, so have to convert it
-    repeating_period_in_seconds = r.stft_params.hop_length * repeating_period / a.sample_rate
+    period_simple_seconds = r.stft_params.hop_length * period_simple / a.sample_rate
+    period_complex_seconds = r.stft_params.hop_length * period_complex / a.sample_rate
 
-    print repeating_period, repeating_period_in_seconds
+    print 'simple = ', period_simple,'hops,',  period_simple_seconds, 'seconds'
+    print 'complex = ', period_complex,'hops,',  period_complex_seconds, 'seconds'
 
 if __name__ == '__main__':
     main()
