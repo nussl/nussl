8a423220394876a9657adbcef83a02a9f22bffa5
Prem Seetharaman <prem@u.northwestern.edu>
2017-02-02 01:56:20 -0600

    added a verbose option to rpca since it takes a while to run

--- Demos/demo_rpca.py
+++ Demos/demo_rpca.py
@@ -5,17 +5,18 @@ import nussl
 
 def load_audio(audio_file):
     print 'Loading %s' % audio_file
-    audio, sr = librosa.load(audio_file, sr=None, mono=True)
+    audio, sr = librosa.load(audio_file, sr=None, mono=True, offset = 90, duration = 30)
     pad_length = (audio.shape[-1] / 1024.0 % 1) * 1024
     #audio = np.pad(np.array(audio), ((0, int(pad_length))), 'constant', constant_values = (0, 0))
     audio_signal = nussl.AudioSignal(audio_data_array = audio, sample_rate = sr)
     audio_signal.stft_params.hop_length = 256
     audio_signal.stft_params.window_length = 1024
+    audio_signal.write_audio_to_file('demo_mixture.wav')
     return audio_signal
 
-mixture = load_audio('./Input/titon_2_07_SNR5.wav')
-rpca = nussl.RPCA(mixture)
+mixture = load_audio('../Input/mixture.wav')
+rpca = nussl.RPCA(mixture, verbose = True)
 rpca.run()
 bg, fg = rpca.make_audio_signals()
-fg.write_audio_to_file('foreground_rpca_titon.wav')
-bg.write_audio_to_file('background_rpca_titon.wav')
+fg.write_audio_to_file('foreground_rpca.wav')
+bg.write_audio_to_file('background_rpca.wav')
