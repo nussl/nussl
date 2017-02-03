fb6dd2f2849a9346c2ecf8cf900f1d80e971e0f1
Prem Seetharaman <prem@u.northwestern.edu>
2017-02-03 02:51:37 -0600

    edited some demos

--- demos/demo_repet_sim.py
+++ demos/demo_repet_sim.py
@@ -6,30 +6,39 @@ if not path in sys.path:
     sys.path.insert(1, path)
 
 import nussl
-
+print nussl.USE_LIBROSA_STFT
+import numpy as np
 
 def main():
     # input audio file
-    input_name = os.path.join('..', 'Input', 'guitar2__cowbell1_none_0.0.wav')
+    input_name = os.path.join('..', 'input','demo_mixture.wav')
     signal = nussl.AudioSignal(path_to_input_file=input_name)
-
+    #signal.to_mono(overwrite = True)
+    original_length = signal.signal_length
+    signal.stft_params.window_length = 2048
+    signal.stft_params.n_fft_bins = 2048
+    signal.stft_params.hop_length = 512
+    print signal.num_channels
     # make a directory to store output if needed
-    if not os.path.exists('../Output/'):
-        os.mkdir('../Output')
+    if not os.path.exists('output/'):
+        os.mkdir('output/')
 
     # Set up Repet
-    repet_sim = nussl.RepetSim(signal)
+    repet = nussl.RepetSim(signal)
+
     # and Run
-    repet_sim.plot(os.path.join('..', 'Output', 'guitar2__cowbell1_similarity_matrix_lib.png'))
-    repet_sim.run()
+    repet.run()
 
-    # Get foreground and background audio signals
-    bkgd, fgnd = repet_sim.make_audio_signals()
-
+    # Get foreground and backgroun audio signals
+    bkgd, fgnd = repet.make_audio_signals()
+    print original_length, signal.signal_length, bkgd.signal_length, fgnd.signal_length
     # and write out to files
-    bkgd.write_audio_to_file(os.path.join('..', 'Output', 'guitar2__cowbell1_background_libstft.wav'))
-    fgnd.write_audio_to_file(os.path.join('..', 'Output', 'guitar2__cowbell1_foreground_libstft.wav'))
-
+    bkgd.write_audio_to_file(os.path.join('output', 'mix1_bg.wav'))
+    fgnd.write_audio_to_file(os.path.join('output', 'mix1_fg.wav'))
+    print bkgd._active_start, fgnd._active_start
+    print bkgd._active_end, fgnd._active_end
+    audio_sum =  bkgd + fgnd
+    audio_sum.write_audio_to_file(os.path.join('output', 'sum.wav'))
 
 if __name__ == '__main__':
     main()
