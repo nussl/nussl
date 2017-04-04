f4e038f7ca3a653c066fcb4bd5b521bf3c945e19
Ethan Manilow <eth@nmanilow.com>
2017-04-04 09:52:57 -0500

    adding test files

---
+++ tests/duet_matlab_files/twoDsmooth.m
@@ -0,0 +1,39 @@
+function smat  =  twoDsmooth (mat , ker )
+%TWO 2 SMOOTH?Smooth 2D matrix.
+% smat = twoDsmooth(mat, ker)
+%
+% MAT is the 2D matrix to be smoothed.
+% KER is either
+%   (1)  a scalar, in which case a ker?by?ker matrix of
+%        1/ker?2 is used as the matrix averaging kernel
+%   (2)  a matrix which is used as the averaging kernel.
+%
+% SMAT is the smoothed matrix (same size as mat).
+if prod(size(ker))==1, % if ker is a scalar
+    kmat = ones(ker, ker)/ker^2;
+else
+    kmat = ker;
+end
+
+% make  kmat  have  odd  dimensions
+[kr  kc] = size(kmat); if rem(kr, 2) == 0,
+    kmat = conv2(kmat, ones (2 ,1))/2;
+    kr  =  kr  +  1;
+end if rem(kc, 2)==0,
+    kmat = conv2(kmat, ones(1, 2))/2;
+    kc = kc + 1;
+end
+
+[mr mc] = size(mat);
+fkr = floor(kr/2); % number  of  rows  to  copy  on  top  and  bottom
+fkc = floor(kc/2); % number  of  columns  to  copy  on  either  side
+smat  =  conv2 (...
+    [mat(1,1)*ones(fkr, fkc)  ones(fkr, 1)*mat(1, :)...
+    mat(1, mc)*ones(fkr, fkc);
+    mat(:, 1)*ones(1, fkc) mat mat(:, mc)*ones(1, fkc)
+    mat(mr, 1)*ones(fkr, fkc) ones(fkr, 1)*mat(mr, :)...
+    mat(mr, mc)*ones(fkr, fkc)],...
+    flipud(fliplr(kmat)), 'valid');
+
+end
+
