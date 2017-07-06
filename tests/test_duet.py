19c83941b101dfec5216783fd5a1f695eaa7edfc
Daniel Felix-Kim <daniel.felixkim@gmail.com>
2017-07-06 12:31:05 -0500

    Added duet_benchmarks folder for unit test values and saved MATLAB tests for later.

--- tests/test_duet.py
+++ tests/test_duet.py
@@ -22,25 +22,25 @@ class DuetUnitTests(unittest.TestCase):
     #     duet = nussl.Duet(signal, 3)
     #     duet.run()
 
-    def test_duet_outputs(self):
-        input_file_name = os.path.join('..', 'input', 'dev1_female3_inst_mix.wav')
-        signal = nussl.AudioSignal(path_to_input_file=input_file_name)
-        refact_duet = nussl.Duet(signal, num_sources=3)
-        refact_duet_result = refact_duet.run()
-        duet = nussl.Duet(signal, 3)
-        nussel_duet_result = duet.run()
-        assert refact_duet_result == nussel_duet_result
-
-    @staticmethod
-    def _load_matlab_results():
-       
-        f_mat_path = os.path.join('duet_reference', 'rickard_duet', 'fmat')
-        tf1_path = os.path.join('duet_reference', 'rickard_duet', 'tf1')
-        tf2_path = os.path.join('duet_reference', 'rickard_duet', 'tf2')
-        
-        fmat = scipy.io.loadmat(f_mat_path)['back'].T
-        delay_mat = scipy.io.loadmat(delay_mat_path)['fore'].T
-        return sym_atn_mat, delay_mat
+    # def test_duet_outputs(self):
+    #     input_file_name = os.path.join('..', 'input', 'dev1_female3_inst_mix.wav')
+    #     signal = nussl.AudioSignal(path_to_input_file=input_file_name)
+    #     refact_duet = nussl.Duet(signal, num_sources=3)
+    #     refact_duet_result = refact_duet.run()
+    #     duet = nussl.Duet(signal, 3)
+    #     nussel_duet_result = duet.run()
+    #     assert refact_duet_result == nussel_duet_result
+    #
+    # @staticmethod
+    # def _load_matlab_results():
+    #
+    #     f_mat_path = os.path.join('duet_reference', 'rickard_duet', 'fmat')
+    #     tf1_path = os.path.join('duet_reference', 'rickard_duet', 'tf1')
+    #     tf2_path = os.path.join('duet_reference', 'rickard_duet', 'tf2')
+    #
+    #     fmat = scipy.io.loadmat(f_mat_path)['back'].T
+    #     delay_mat = scipy.io.loadmat(delay_mat_path)['fore'].T
+    #     return sym_atn_mat, delay_mat
 
     def test_compute_spectrogram_1_channel(self):
         # Test with one channel, should throw value error
@@ -54,23 +54,23 @@ class DuetUnitTests(unittest.TestCase):
     def test_compute_spectrogram_wmat(self):
         # Load MATLAB values
         # f_mat_path = os.path.join('duet_reference', 'rickard_duet', 'fmat')
-        tf1_path = os.path.join('duet_reference', 'rickard_duet', 'tf1')
-        tf2_path = os.path.join('duet_reference', 'rickard_duet', 'tf2')
+        stft0_path = os.path.join('duet_reference', 'duet_benchmarks', 'benchmark_stft_ch0.npy')
+        stft1_path = os.path.join('duet_reference', 'duet_benchmarks', 'benchmark_stft_ch1.npy')
 
         # fmat = scipy.io.loadmat(f_mat_path)['fmat']
-        tf1_mat = scipy.io.loadmat(tf1_path)['tf1']
-        tf2_mat = scipy.io.loadmat(tf2_path)['tf2']
+        benchmark_stft0 = np.load(stft0_path)
+        benchmark_stft1 = np.load(stft1_path)
 
         path = os.path.join('..', 'Input', 'dev1_female3_inst_mix.wav')
         signal = nussl.AudioSignal(path)
         duet = nussl.Duet(signal, 3)
-        duet.stft_params.window_length = 1024
-        duet.stft_params.window_type = nussl.WINDOW_RECTANGULAR
-        duet.stft_params.hop_length = duet.stft_params.window_length
+        # duet.stft_params.window_length = 1024
+        # duet.stft_params.window_type = nussl.WINDOW_RECTANGULAR
+        # duet.stft_params.hop_length = duet.stft_params.window_length
 
         duet_sft0, duet_sft1, duet_wmat = duet._compute_spectrogram(duet.sample_rate)
-        assert np.allclose(duet_sft0, tf1_mat, atol=1e-06)
-        assert np.allclose(duet_sft1, tf2_mat, atol=1e-06)
+        assert np.all(benchmark_stft0 == duet_sft0)
+        assert np.all(benchmark_stft0 == duet_sft1)
         # assert np.allclose(duet_wmat, fmat)
 
     # def test_compute_atn_delay(self):
