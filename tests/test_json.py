4125d9631a889c002991a3bb705a3de3b38c883c
ethman <ethanmanilow@gmail.com>
2017-09-11 16:40:05 -0500

    Fixing to-from json tests.

--- tests/test_json.py
+++ tests/test_json.py
@@ -51,7 +51,6 @@ class TestJson(unittest.TestCase):
         path = os.path.join('..', 'Input', 'mix1.wav')
         a = nussl.AudioSignal(path)
         r = nussl.RepetSim(a)
-        t = nussl.Repet(a)
         r()
 
         j = r.to_json()
