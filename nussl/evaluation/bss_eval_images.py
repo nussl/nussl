ff863e2d3ceda21ff813d2a1331b646e87ffbf91
ethman <ethanmanilow@gmail.com>
2017-09-16 18:22:47 -0500

    Updating BSS-Eval methods. Version 1

--- nussl/evaluation/bss_eval_images.py
+++ nussl/evaluation/bss_eval_images.py
@@ -1,37 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-
+TODO
 """
 
-import numpy as np
 import mir_eval
 
 import bss_eval_base
 
 
-class BSSEvalSources(bss_eval_base.BSSEvalBase):
+class BSSEvalImages(bss_eval_base.BSSEvalBase):
     """
 
     """
 
     def __init__(self, true_sources_list, estimated_sources_list, source_labels=None, algorithm_name=None,
                  do_mono=False, compute_permutation=True):
-        super(BSSEvalSources, self).__init__(true_sources_list=true_sources_list,
+        super(BSSEvalImages, self).__init__(true_sources_list=true_sources_list,
                                              estimated_sources_list=estimated_sources_list,
                                              source_labels=source_labels, do_mono=do_mono,
                                              compute_permutation=compute_permutation)
 
-        self.mir_eval_func = mir_eval.separation.bss_eval_sources
+        self.mir_eval_func = mir_eval.separation.bss_eval_images
 
 
     def _preprocess_sources(self):
-        reference, estimated = super(BSSEvalSources, self)._preprocess_sources()
+        reference, estimated = super(BSSEvalImages, self)._preprocess_sources()
 
-        # TODO: Check this - not sure what's going on...
-        if self.num_channels != 1:
-            reference = np.sum(reference, axis=-1)
-            estimated = np.sum(estimated, axis=-1)
+        if self.num_channels == 1:
+            raise Exception("Can't run bss_eval_images on mono audio signals!")
 
         mir_eval.separation.validate(reference, estimated)
 
