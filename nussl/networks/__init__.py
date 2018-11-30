d35794e2bd682fcf6003356eb9f0908a8726337b
Prem Seetharaman <prem@u.northwestern.edu>
2018-11-29 23:46:10 -0600

    fail gracefully without torch

--- nussl/networks/__init__.py
+++ nussl/networks/__init__.py
@@ -1,1 +1,7 @@
-from .separation_model import SeparationModel
+from .. import torch_imported, ImportErrorClass
+if torch_imported:
+    from .separation_model import SeparationModel
+else:
+    class SeparationModel(ImportErrorClass):
+        def __init__(self):
+            super(SeparationModel, self).__init__('pytorch')
