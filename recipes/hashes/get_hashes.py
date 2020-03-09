ae97d5e2265681a9940fe4b04cd4c00ddab58a73
pseeth <prem@u.northwestern.edu>
2020-03-09 19:13:47 +0000

    indent in json

--- recipes/hashes/get_hashes.py
+++ recipes/hashes/get_hashes.py
@@ -5,7 +5,7 @@ with open('musdb_hashes.json', 'w') as f:
     hashes = {}
     hash_ = efz_utils._hash_directory('/home/data/musdb/raw/musdb_unzip')
     hashes['musdb'] = hash_
-    json.dump(hashes, f)
+    json.dump(hashes, f, indent=4)
 
 with open('wham_hashes.json', 'w') as f:
     hashes = {}
@@ -16,4 +16,4 @@ with open('wham_hashes.json', 'w') as f:
         'wav8k': wav8k_hash,
         'wav16k': wav16k_hash
     }
-    json.dump(hashes, f)
+    json.dump(hashes, f, indent=4)
