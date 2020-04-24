16dab17cf6a555bd1b1428df9592946c96cdf548
pseeth <prem@u.northwestern.edu>
2020-04-24 05:51:39 +0000

    bumping version

--- nussl/__init__.py
+++ nussl/__init__.py
@@ -5,7 +5,7 @@ except Exception:
     vamp_imported = False
 
 # Current nussl version
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 class ImportErrorClass(object):
     def __init__(self, lib, **kwargs):
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('extra_requirements.txt') as f:
 
 setup(
     name='nussl',
-    version='1.0.1',
+    version='1.1.0',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
