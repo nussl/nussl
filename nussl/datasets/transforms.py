2253a110b9ddc554fad7842d682d0263e5d9ddf3
abugler <abugler@gmail.com>
2020-05-08 17:52:03 +0000

    deleted old augment in transforms.py

--- nussl/datasets/transforms.py
+++ nussl/datasets/transforms.py
@@ -750,53 +750,6 @@ class ToSeparationModel(object):
     def __repr__(self):
         return f"{self.__class__.__name__}()"
 
-class Augment:
-    """
-    The Augment transform will take a dictionary, with the keys ``mix_key``
-    and ``source_key``, which default to `mix` and `source` respectively.
-    The augment transformation may include any of the following:
-
-    Time stretching: Linearly scale the time axis in respect to a central portion. 
-    Pitch shifting: Increase or decrease the sounds of the AudioSignal by a number of half steps
-    Remixing: Change the loudness of each source independently.
-    Loudness Scaling: Change the loudness of all sources by the same magnitude.
-    Inverse gaussian filtering: Multiply each tf bin by the factor
-    (1 - Gaussian(f)), where f is frequency, and Gaussian's parameters 
-    are mean frequency, and standard deviation.
-    Tremolo: Apply a Tremolo Effect to all sources and mixtures.
-    Vibrato: Apply a Vibrato Effect to all sources and mixtures.
-
-    The augmentations can be choosen via kwargs, where each key of kwargs is one of the following augments, 
-    and the value represents a ranges of randomly chosen values.  
-
-        The following are tuples of length 2.
-        time_stretch: Indicates range of factors for the time stretch, where (min_stretch, max_stretch)
-        pitch_shift: Indicates range of shifts by number of half steps for the pitch shift, where (min_shift, max_shift)
-        remix: Indicates range of factors for independent loudness scaling premixing, where (min_factor, max_factor)
-        loudness_scale: Indicates range of factors for loudness scalings, where (min_factor, max_factor)
-        low_pass: Indicates range of thresholds for low pass filters where (min_threshold, max_thresold)
-        high_pass: Indicates range of thresholds for high pass filters where (min_threshold, max_threshold)
-
-        The following are tuples of length 2, where each element is also a tuple of length 2. 
-        tremolo: Indicates ranges for parameters to tremolo function, where ((min_modulation_frequency, max_modulation_frequency)
-            ,(min_modulation_depth, max_modulation_depth))
-        vibrato: Indicates ranges for parameters to vibrato function, where ((min_modulation_frequency, max_modulation_frequency)
-            ,(min_modulation_depth, max_modulation_depth))
-        igaussian_filter: Indicates ranges for parameters to the Inverse Gaussian, where ((min_mean, max_mean), 
-            (min_standard_deviation, max_standard_derivations))
-    """
-    def __init__(self, mix_key='mix', source_key='source', **kwargs):
-        self.mix_key = mix_key
-        self.source_key = source_key
-
-        if not kwargs:
-            raise ValueError("No augmentations in kwargs were passed")
-
-        self.augmentations = kwargs
-
-    def _random_range(low, high):
-        return np.random.rand() * (high - low) + low
-
 class Compose(object):
     """Composes several transforms together. Inspired by torchvision implementation.
 
@@ -836,10 +789,5 @@ class TransformException(Exception):
     """
     pass
 
-## TODO: Finish all the augmented transforms
 
-def _copy_meta_data(new_dict, old_dict):
-    for key, value in old_dict.items():
-        if key != "sources" and key != "mix":
-            new_dict[key] = value
 
