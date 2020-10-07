7439e7e5937086f59d0c4a9c93cb59a5907cf02d
pseeth <prem@descript.com>
2020-10-07 16:46:09 -0700

    Bumping version.

--- nussl/__init__.py
+++ nussl/__init__.py
@@ -5,7 +5,7 @@ except Exception:
     vamp_imported = False
 
 # Current nussl version
-__version__ = '1.1.3rc4'
+__version__ = '1.1.3rc5'
 
 class ImportErrorClass(object):
     def __init__(self, lib, **kwargs):
