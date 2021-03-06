1cb68abb133bf13cd4ddbde73b947ea45a79b91e
pseeth <prem@descript.com>
2021-03-05 20:30:24 -0800

    Bumping version for staging.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('extra_requirements.txt') as f:
 
 setup(
     name='nussl',
-    version="1.1.7",
+    version="1.1.8",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
