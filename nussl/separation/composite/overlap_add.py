89476cd024d36dc257a4b46e6a3cff77d8cba5b8
Ethan Manilow <eth@nmanilow.com>
2021-06-23 16:00:54 -0500

    Formatting change to trigger tests.

--- nussl/separation/composite/overlap_add.py
+++ nussl/separation/composite/overlap_add.py
@@ -3,25 +3,26 @@ from ... import AudioSignal
 import numpy as np
 import tqdm
 
+
 class OverlapAdd(SeparationBase):
+    """Apply overlap/add to a long audio file to separate it in chunks.
+    Note that if the hop length is not half the window length, COLA
+    may be violated (see https://en.wikipedia.org/wiki/Overlap%E2%80%93add_method
+    for more).
+
+    Args:
+        separation_object (SeparationBase): Separation object that overlap and add is applied to.
+        window_duration (int): Window duration of overlap/add window, by default 15 seconds.
+        hop_duration (int): Hop duration of overlap/add window, by default half the window duration.
+            If the hop duration is not half the window duration, overlap and add
+            may have strange results.
+        window_type (str): Type of window to apply to each segment before performing overlap and add.
+            Note that options other than 'hanning' may not result in perfect overlap and add.
+        find_permutation (bool): Whether or not to find the permutation between chunks before combining.
+        verbose (bool): Whether or not to show a progress bar as file is being separated.
+    """
     def __init__(self, separation_object, window_duration=15, hop_duration=None, window_type='hanning', 
                  find_permutation=False, verbose=False):
-        """Apply overlap/add to a long audio file to separate it in chunks.
-        Note that if the hop length is not half the window length, COLA
-        may be violated (see https://en.wikipedia.org/wiki/Overlap%E2%80%93add_method 
-        for more).
-
-        Args:
-            separation_object (SeparationBase): Separation object that overlap and add is applied to.
-            window_duration (int): Window duration of overlap/add window, by default 15 seconds.
-            hop_duration (int): Hop duration of overlap/add window, by default half the window duration.
-                If the hop duration is not half the window duration, overlap and add
-                may have strange results. 
-            window_type (str): Type of window to apply to each segment before performing overlap and add. 
-                Note that options other than 'hanning' may not result in perfect overlap and add.
-            find_permutation (bool): Whether or not to find the permutation between chunks before combining.
-            verbose (bool): Whether or not to show a progress bar as file is being separated.
-        """
         if hop_duration is None:
             hop_duration = window_duration / 2
 
